d8a5d9c79110562bf5c3f13334057d50add89783
Jonathan Gibbons <gibbons_jonathan@hotmail.com>
2021-04-04 18:00:14 +0100

    Upgraded to akka.typed and sacal 2.13

--- sf-fix-tester/src/test/scala/org/sackfix/tester/tests/SackFixTestSpec.scala
+++ sf-fix-tester/src/test/scala/org/sackfix/tester/tests/SackFixTestSpec.scala
@@ -1,16 +1,17 @@
 package org.sackfix.tester.tests
 
 import org.sackfix.tester.simplefix.sockets.SocketHandler
-import org.scalatest.{BeforeAndAfterEach, FlatSpec}
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.flatspec.AnyFlatSpec
 
 /**
   * Created by Jonathan during 2017.
   */
-class SackFixTestSpec extends FlatSpec with BeforeAndAfterEach {
-  override protected def beforeEach = {
+class SackFixTestSpec extends AnyFlatSpec with BeforeAndAfterEach {
+  override protected def beforeEach() = {
   }
 
-  override protected def afterEach = {
+  override protected def afterEach() = {
     SocketHandler.tidyUpTest
   }
 }
